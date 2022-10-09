b6992de0c983196f32c16ac6bcda2343a77103df
Archit3ct <timothy.s.rasmussen@gmail.com>
2022-10-09 15:25:06 +0200

    project update

--- src/main/java/com/example/mandatorytest/services/PersonRegister.java
+++ src/main/java/com/example/mandatorytest/services/PersonRegister.java
@@ -1,6 +1,8 @@
 package com.example.mandatorytest.services;
 
 import java.text.MessageFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Random;
 
 public class PersonRegister {
@@ -47,7 +49,31 @@ public class PersonRegister {
         // PICK A RANDOM MONTH based on the enum Months
         // As enums start at index 0. I add + 1.
 
+        generateRandomMonth();
+        generateDayFromMonth();
+
+
+
+
+
+
+        // BASED ON THE MONTH PICK A RANDOM DATA AVAILABLE IN THAT MONTH
+        // PICK A YEAR - 70 to - 18  Years from current year.
+
+
+    }
+
+    private void generateRandomYear() {
+        Date d = new Calendar();
+        int year = d.getYear();
+
+    }
+
+    private void generateRandomMonth() {
         month = randomEnum().ordinal() + 1;
+    }
+
+    private void generateDayFromMonth() {
 
         switch (month) {
 
@@ -63,19 +89,36 @@ public class PersonRegister {
             case 4:
                 // Days in April 1-30
                 day = randomWithRange(1, 30);
-               
+            case 5:
+                // Days in May 1-31
+                day = randomWithRange(1, 31);
+            case 6:
+                // Days in June 1-30
+                day = randomWithRange(1, 30);
+            case 7:
+                // Days in July 1-31
+                day = randomWithRange(1, 31);
+            case 8:
+                // Days in August 1-31
+                day = randomWithRange(1, 31);
+            case 9:
+                // Days in September 1-30
+                day = randomWithRange(1, 30);
+            case 10:
+                // Days in October 1-31
+                day = randomWithRange(1, 31);
+            case 11:
+                // Days in November 1-30
+                day = randomWithRange(1, 30);
+            case 12:
+                // Days in December 1-31
+                day = randomWithRange(1, 31);
 
         }
 
-
-
-        // BASED ON THE MONTH PICK A RANDOM DATA AVAILABLE IN THAT MONTH
-        // PICK A YEAR - 70 to - 18  Years from current year.
-
-
     }
 
-    public Months randomEnum() {
+    private Months randomEnum() {
 
         return Months.values()[new Random().nextInt(Months.values().length)];
 
